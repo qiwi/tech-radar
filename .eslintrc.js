689d18d6db594156520e85285bf0a11e4bdb6ea2
pismenskiy <pismenskiy.m@gmail.com>
2021-06-22 09:46:33 +0300

    feat: initial commit

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    extends: [
-        'eslint-config-qiwi',
-        'prettier',
-    ],
-};
--- src/test/js/index.js
+++
@@ -1,7 +0,0 @@
-import foo from '../../main/js/index'
-
-describe('index (es6)', () => {
-  it('foo() result equals bar', () => {
-    expect(foo).toBe(10)
-  })
-})
