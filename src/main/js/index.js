44094150e60f954a9a602bde7f49fd2be4792090
Demian222 <d11133@yandex.ru>
2021-07-01 18:50:40 +0300

    fix: fix conflict

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -40,7 +40,7 @@ export const getDirs = (sources) =>
       let r
       let i = 0
 
-      while (uniq(r).length !== a.length && i < 10) {
+      while (uniq(r).length !== a.length) {
         console.log(r)
         i++
         r = a.map((c) => reverse(c.slice(0, i)).join('-'))
