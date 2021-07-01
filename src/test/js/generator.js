7bbca203dd12059d9a103b5c3eb9ce5f7b78ca5d
Demian222 <d11133@yandex.ru>
2021-07-01 18:47:21 +0300

    fix: fix conflict

--- src/test/js/generator.js
+++ src/test/js/generator.js
@@ -1,41 +1,21 @@
-import {getDirs} from '../../main/js/generator'
+import { getDirs } from '../../main/js/index.js'
 
 describe('getDirs', () => {
   const cases = [
     [
       'returns uniq file names with omitted extensions',
-      [
-      '/foo/bar/baz.csv',
-      '/foo/bar/qux.csv'
-      ],
-      [
-        'baz',
-        'qux'
-      ]
+      ['/foo/bar/baz.csv', '/foo/bar/qux.csv'],
+      ['baz', 'qux'],
     ],
     [
       'append parent dir otherwise',
-      [
-        '/foo/bar/data.csv',
-        '/foo/baz/data.json'
-      ],
-      [
-        'bar-data',
-        'baz-data'
-      ]
+      ['/foo/bar/data.csv', '/foo/baz/data.json'],
+      ['bar-data', 'baz-data'],
     ],
     [
       'makes values unique',
-      [
-        '/foo/bar/data.csv',
-        '/foo/bar/data.json',
-        '/foo/bar/data.yaml'
-      ],
-      [
-        'data',
-        'data-2',
-        'data-3',
-      ]
+      ['/foo/bar/data.csv', '/foo/bar/data.json', '/foo/bar/data.yaml'],
+      ['data', 'data-2', 'data-3'],
     ],
     [
       'makes values unique',
@@ -43,15 +23,10 @@ describe('getDirs', () => {
         '/foo/ios/bar/data.csv',
         '/foo/ios/bar/data.json',
         '/foo/js/bar/data.yaml',
-        '/foo/js/bar/data.yml'
+        '/foo/js/bar/data.yml',
       ],
-      [
-        'ios-bar-data',
-        'ios-bar-data-2',
-        'js-bar-data',
-        'js-bar-data-2',
-      ]
-    ]
+      ['ios-bar-data', 'ios-bar-data-2', 'js-bar-data', 'js-bar-data-2'],
+    ],
   ]
 
   cases.forEach(([name, input, result]) => {
