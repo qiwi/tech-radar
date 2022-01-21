38ce5ecd023cefecfc6c7490ab5ede2e110c0ee8
Anton Golub <antongolub@antongolub.com>
2022-01-21 23:31:48 +0300

    feat: add template view

--- src/main/js/parser/index.js
+++ src/main/js/parser/index.js
@@ -57,26 +57,38 @@ export const getSources = async (pattern, cwd) =>
     cwd,
   })
 
-export const normalizeQuadrantAliases = (aliases) => Object.entries(aliases).reduce((m, [k, v]) => {
-  if (/^q[1-4]$/.test(k)) {
-    asArray(v).forEach(_v => {
-      m[_v] = k
-    })
-  } else {
-    m[k] = v
-  }
+export const normalizeQuadrantAliases = (aliases) =>
+  Object.entries(aliases).reduce((m, [k, v]) => {
+    if (/^q[1-4]$/.test(k)) {
+      asArray(v).forEach((_v) => {
+        m[_v] = k
+      })
+    } else {
+      m[k] = v
+    }
 
-  return m
-}, {})
+    return m
+  }, {})
+
+export const normalizeQuadrantTitles = (titles) => ({
+  q1: 'Q1',
+  q2: 'Q2',
+  q3: 'Q4',
+  q4: 'Q4',
+  ...titles,
+})
 
 export const normalizeEntries = (doc) => {
   doc.quadrantAliases = normalizeQuadrantAliases(doc.quadrantAliases)
+  doc.quadrantTitles = normalizeQuadrantTitles(doc.quadrantTitles)
 
   doc.data.forEach((entry) => {
     entry.ring = entry.ring.toLowerCase()
     entry.quadrant = getQuadrant(entry.quadrant, doc.quadrantAliases)
+    entry.quadrantTitle = doc.quadrantTitles[entry.quadrant]
     entry.moved = +entry.moved || 0
   })
+  doc.data.sort((a, b) => (a.name > b.name ? 1 : -1))
 
   return doc
 }
