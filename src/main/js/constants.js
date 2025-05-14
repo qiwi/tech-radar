2a01d6ec546b70251b2cca1156e1c9b579daaf04
Anton Golub <antongolub@antongolub.com>
2025-05-14 12:22:29 +0300

    perf: update eleventy to v3.1.0

--- src/main/js/constants.js
+++ src/main/js/constants.js
@@ -1,5 +1,5 @@
-import path, { dirname } from 'node:path'
+import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-export const rootDir = dirname(fileURLToPath(import.meta.url))
+export const rootDir = path.dirname(fileURLToPath(import.meta.url))
 export const tplDir = path.resolve(rootDir, '../tpl')
